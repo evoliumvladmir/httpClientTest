8a3b079c2b0455bdc763de92b855e94fc68d54b3
evoliumvladmir <valdemiro.cachinguembi@evolium.ao>
2026-06-24 14:32:39 +0100

    card style

--- src/app/components/products-card/products-card.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-products-card',
-  imports: [],
-  templateUrl: './products-card.html',
-  styleUrl: './products-card.css',
-})
-export class ProductsCard {}
--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 /* service import */
-import {ProductService} from '../../services/product'
+import { ProductService } from '../../services/product'
+
+/* pipes import */
+import {CurrencyPipe} from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products implements OnInit {
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService) { }
 
   products: any[] = []
 
   ngOnInit() {
     this.productService.getProducts().subscribe(data => {
       this.products = data
+      console.log(data)
     })
   }
 }
